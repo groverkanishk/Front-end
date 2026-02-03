e11614b1540548b9512b1c78556e68198305e6b2
Kanishk Grover <2501060071@krmu.edu.in>
2026-02-03 09:51:36 +0530

    .

--- Day 9/script.js
+++ Day 9/script.js
@@ -1,3 +1,15 @@
 const query = document.querySelector('#content p')
 console.log(query)
 query.textContent = "fkjefejwkv jefhewhf"
+
+const h1heading = document.getElementsByTagName("h1")
+for(let i = 0; i < h1heading.length; i++){
+    h1heading[i].style.color = "tomato"
+}
+
+const query2 = document
+console.log(query2, "query");
+
+for(let i = 0; i < query2.length; i++){
+    query2[i].textContent = "hafjkhfka vfialfh"
+}
